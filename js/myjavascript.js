4693a2789fadd0391df79f50014376d2074c0cee
najlaaouda <najlaaouda.88@gmail.com>
2024-06-28 02:06:01 +0300

    personal

---
+++ js/myjavascript.js
@@ -0,0 +1,58 @@
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()>100){
+            $("#btn-arrow-up").fadeIn('slow');
+                        }
+                        else
+                        $("#btn-arrow-up").fadeOut('slow');
+
+    })
+    $("#btn-arrow-up").click(function(){
+        $("html, body").animate({scrollTop:0, 
+        }, 800)
+        return false;
+    })    
+ })
+
+ $(document).ready(function () {
+  
+    $('ul.navbar-nav > li').click(function (e) {
+        // $('ul.navbar-nav > li') .removeClass('active');
+
+        // var x = $('ul.navbar-nav > li').offset().top;
+    //    $('html,body').animate({scrollTop:},2000);
+        $(this).addClass('active');
+    
+    });
+});
+
+
+$(document).ready(function(){
+
+    if ($('.text-slider').length == 1) {
+      var typed_strings = $('.text-slider-items').text();
+      var typed = new Typed('.text-slider', {
+        strings: typed_strings.split(','),
+        typeSpeed: 80,
+        loop: true,
+        backDelay: 1100,
+        backSpeed: 30
+      });
+    }
+
+    })
+  
+const secskill = document.querySelector('.skills');
+const progressBar = document.querySelectorAll('.progress-skill span')
+window.onscroll = function(){
+    if(window.scrollY>= secskill.offsetTop -100){
+        console.log("hhh")
+        progressBar.forEach((ele)=>{
+            ele.style.width = ele.dataset.width
+
+        })
+
+    }
+}
